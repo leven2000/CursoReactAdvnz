96d47338e2e11ab682808518a81cbefdc5f2fd21
Tiago <tiagobrizuela18@gmail.com>
2023-09-20 14:32:43 -0300

    LazyLoadBasico

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,43 @@
+import { Suspense } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, NavLink, Navigate} from "react-router-dom";
+import { routes } from "./routes";
+
+import logo from "../logo.svg"
+
+
+
+
+export const Navigation = () => {
+  return (
+   
+    <Suspense fallback={<span>Cargando...</span>}>
+        <BrowserRouter>
+            <div className="main-layout">
+                <nav>
+                    <img src={logo } alt="react logo"/>            
+                    <ul>
+                        {
+                            routes.map(route =>(
+                                <li key={route.to}>
+                                    <NavLink to={route.to} className={ ({isActive}) => isActive? "nav-active": "" } >{route.name}</NavLink>
+                                </li>   
+                            ))
+                        }                                   
+                    </ul>
+                </nav>
+
+                <Routes>                
+                    {
+                        routes.map(({Component, path}) =>(
+                            <Route key={path} path={path} element={<Component/>}/>
+                        ))
+                    }                                
+                    <Route path="/*" element={<Navigate to={routes[0].to } replace />}/>                    
+                </Routes>
+            </div>
+        </BrowserRouter>
+    </Suspense>    
+    
+  )
+}
